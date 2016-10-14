c50a7834c9cf9ee2db03a1d01a18e9fcc0e11f86
hlif <hlifh15>
2016-10-14 22:16:51 +0000

    Kata step 1, setti inn add fall

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,10 +1,16 @@
 package is.ru.stringcalculator;
+package is.ru.stringcalculator;
+import java.util.ArrayList;
 
 public class Calculator {
 
 	public static int add(String text){
-	
+		if(text.equals("")){
 			return 0;
 		}
-
-}
+		else if(text.contains(",")){
+			return sum(splitNumbers(text));
+		}
+		else
+			return 1;
+	}
